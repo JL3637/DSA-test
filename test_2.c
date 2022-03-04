22c3fc9624eb3ba75460bade57b361ee4b14a240
jason <jason36375667@gmail.com>
2022-03-05 00:50:21 +0800

    great job

--- test_2.c
+++ test_2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#define arrlen 15
 
 void print_board(int *board, int n, int m){
     int a = 0;
@@ -10,9 +11,6 @@ void print_board(int *board, int n, int m){
             else if(*(board + a) == 0){
                 printf("_");
             }
-            else if(*(board + a) == -1){
-                printf("x");
-            }
             a++;
         }
         printf("\n");
@@ -23,7 +21,7 @@ int check_valid_board(int *board, int n, int m, int *row_block_number, int *colu
     for(int i = 0; i < n; i++){
         int blocks_sum = 0;
         int cells_sum = 0;
-        int b = 20 * i;
+        int b = arrlen * i;
         int index = 0;
         for(int j = 0; j < m; j++){
             if(*(board + (m * i + j)) == 1 && index == 0){
@@ -62,10 +60,10 @@ int check_valid_board(int *board, int n, int m, int *row_block_number, int *colu
     for(int i = 0; i < m; i++){
         int blocks_sum = 0;
         int cells_sum = 0;
-        int b = 20 * i;
+        int b = arrlen * i;
         int index = 0;
         for(int j = 0; j < n; j++){
-            if(*(board + (i + n * j)) == 1 && index == 0){
+            if(*(board + (i + m * j)) == 1 && index == 0){
                 blocks_sum++;
                 cells_sum++;
                 index = 1;
@@ -75,7 +73,7 @@ int check_valid_board(int *board, int n, int m, int *row_block_number, int *colu
                     }
                 }
             }
-            else if(*(board + (i + n * j)) == 1 && index == 1){
+            else if(*(board + (i + m * j)) == 1 && index == 1){
                 cells_sum++;
                 if(j == n - 1){
                     if(cells_sum != *(column_cells + b)){
@@ -83,7 +81,7 @@ int check_valid_board(int *board, int n, int m, int *row_block_number, int *colu
                     }
                 }
             }
-            else if(*(board + (i + n * j)) == 0 && index == 1){
+            else if(*(board + (i + m * j)) == 0 && index == 1){
                 if(cells_sum != *(column_cells + b)){
                     return 0;
                 }
@@ -101,29 +99,22 @@ int check_valid_board(int *board, int n, int m, int *row_block_number, int *colu
     return 1;
 }
 
-int check_board_filled(int *board, int n, int m){
-    for(int i = 0; i < n * m; i++){
-        if(*(board + i) == -1){
-            return 0;
-        }
-    }
-    return 1;
-}
-
-void paint_board(int *board, int position, int color){
+void paint_board(int *board, int *position, int color){
     if(color == 1){
-        *(board + position) = 1;
+        *(board + *position) = 1;
+        *position += 1;
     }
-    if(color == 0){
-        *(board + position) = 0;
+    else if(color == 0){
+        *(board + *position) = 0;
+        *position += 1;
     }
-    if(color == -1){
-        *(board + position) = -1;
+    else if(color == -1){
+        *(board + *position) = -1;
     }
 }
 
-int recursion(int *board, int n, int m, int *row_block_number, int *column_block_number, int *row_cells, int *column_cells){
-    if(check_board_filled(board, n, m)){
+int recursion(int *board, int n, int m, int *row_block_number, int *column_block_number, int *row_cells, int *column_cells, int *position){
+    if(*(board + n * m - 1) != -1){
         if(check_valid_board(board, n, m, row_block_number, column_block_number, row_cells, column_cells)){
             print_board(board, n, m);
             return 1;
@@ -133,22 +124,21 @@ int recursion(int *board, int n, int m, int *row_block_number, int *column_block
         }
     }
     else{
-        int position = 0;
-        for(int i = 0; i < n * m; i++){
-            if(*(board + position) == -1){
-                break;
-            }
-            position++;
-        }
         paint_board(board, position, 1);
-        int succeed = recursion(board, n, m, row_block_number, column_block_number, row_cells, column_cells);
+        int succeed = recursion(board, n, m, row_block_number, column_block_number, row_cells, column_cells, position);
         if(succeed){
             return 1;
+        }
+        else{
+            *position -= 1;
         }
         paint_board(board, position, 0);
-        succeed = recursion(board, n, m, row_block_number, column_block_number, row_cells, column_cells);
+        succeed = recursion(board, n, m, row_block_number, column_block_number, row_cells, column_cells, position);
         if(succeed){
             return 1;
+        }
+        else{
+            *position -= 1;
         }
         paint_board(board, position, -1);
         return 0;
@@ -160,16 +150,16 @@ int main(){
     scanf("%d%d", &n, &m);
     int row_block_number[n];
     int column_block_number[m];
-    int row_cells[n][20];
+    int row_cells[n][arrlen];
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < 20; j++){
+        for(int j = 0; j < arrlen; j++){
            row_cells[i][j] = 0; 
         }
     }
-    int column_cells[m][20];
+    int column_cells[m][arrlen];
     for(int i = 0; i < m; i++){
-        for(int j = 0; j < 20; j++){
-           row_cells[i][j] = 0; 
+        for(int j = 0; j < arrlen; j++){
+           column_cells[i][j] = 0; 
         }
     }
     for(int i = 0; i < n; i++){
@@ -192,7 +182,9 @@ int main(){
         }
     }
 
-    recursion(&board[0][0], n, m, &row_block_number[0], &column_block_number[0], &row_cells[0][0], &column_cells[0][0]);
+    int position = 0;
+
+    recursion(&board[0][0], n, m, &row_block_number[0], &column_block_number[0], &row_cells[0][0], &column_cells[0][0], &position);
     
     return 0;
 }
